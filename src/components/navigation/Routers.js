7d9ad142f51b11020f74a6d48494837eafbd74b8
foreverchoi0706 <57241273+foreverchoi0706@users.noreply.github.com>
2020-11-10 22:49:35 +0900

    라우터기능구현

--- src/components/navigation/Routers.js
+++ src/components/navigation/Routers.js
@@ -4,14 +4,68 @@ const RoutersStyled = styled.div`
   position: absolute;
   background-color: rgba(255, 255, 255, 0.8);
   width: 100%;
-  height: 300px;
+  height: 200px;
+  top: 0px;
   right: 0px;
-  cursor: pointer;
+
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  a {
+    font-size: 1.5rem;
+    color: black;
+  }
+
+  .Router-close {
+    position: absolute;
+    font-size: 2rem;
+    top: 20px;
+    right: 30px;
+    border: none;
+    color: red;
+    font-weight: bold;
+    cursor: pointer;
+    background: none;
+  }
+
+  @media (max-width: 768px) {
+    height: 150px;
+    a {
+      font-size: 1rem;
+    }
+    .Router-close {
+      font-size: 1.5rem;
+      top: 15px;
+      right: 20px;
+    }
+  }
+  @media (max-width: 512px) {
+    height: 100px;
+    a {
+      font-size: 0.7rem;
+    }
+    .Router-close {
+      font-size: 1rem;
+      top: 10px;
+      right: 10px;
+    }
+  }
 `;
 
 const Routers = ({ isClicked, handleClick }) => {
   if (isClicked) {
-    return <RoutersStyled onClick={handleClick}></RoutersStyled>;
+    return (
+      <RoutersStyled>
+        <a href="#root">#Top</a>
+        <a href="#intro">#Intro</a>
+        <a href="#skills">#SKILL STACKS</a>
+        <a href="#projects">#Projects</a>
+        <a href="#contact">#CONTACT ME?</a>
+        <button className="Router-close" onClick={handleClick}>
+          X
+        </button>
+      </RoutersStyled>
+    );
   } else {
     return null;
   }
