bf216a8261db03a13104bec06b34d4ac9323e7f9
foreverChoi <foreverchoi0706@gmail.com>
2021-01-24 13:58:32 +0900

    init

--- src/components/App.js
+++ src/components/App.js
@@ -1,69 +1,8 @@
-import React, { useEffect, useState } from "react";
-import SlideContainerStyled from "./SlideContainer";
-import SlideBtn from "./SlideBtn";
-import ShowProcessBar from "./ShowProcessBar";
-import styled from "styled-components";
-
-const AppStyled = styled.main`
-  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
-  overflow-x: hidden;
-  position: relative;
-  & ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    li > section {
-      //모든 섹션에 좌우패딩값줌
-      padding: 0px 10vw 0px 10vw;
-    }
-  }
-`;
+import React from "react";
+import { memo } from "react";
 
 const App = () => {
-  const [isShow, setIsShow] = useState(true);
-
-  const [slidePosition, setSlidePotion] = useState(0);
-
-  useEffect(() => {
-    if (window.innerWidth < 1028) {
-      setIsShow(false);
-    }
-    window.addEventListener("resize", () => {
-      if (window.innerWidth > 1028) {
-        //1028px이하면 가로모드
-        setIsShow(true);
-      } else {
-        //1028px이하면 세로모드
-        setIsShow(false);
-        setSlidePotion(0);
-      }
-    });
-  }, [window]);
-
-  const slideLeft = () => {
-    setSlidePotion(slidePosition + 1);
-  };
-
-  const slideRight = () => {
-    setSlidePotion(slidePosition - 1);
-  };
-
-  return (
-    <AppStyled>
-      <SlideContainerStyled isShow={isShow} slidePosition={slidePosition} />
-      {isShow && <ShowProcessBar slidePosition={slidePosition} />}
-      <SlideBtn
-        clickSlideBtn={slideLeft}
-        division={"LEFT"}
-        slidePosition={slidePosition}
-      />
-      <SlideBtn
-        clickSlideBtn={slideRight}
-        division={"RIGHT"}
-        slidePosition={slidePosition}
-      />
-    </AppStyled>
-  );
+    return <div>App</div>
 };
 
-export default App;
+export default memo(App);
