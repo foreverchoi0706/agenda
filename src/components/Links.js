f271baab6a6b2e7964ab402d5e14b89ae7d1440a
foreverChoi <foreverchoi0706@gmail.com>
2021-02-04 21:19:03 +0900

    link fix

--- src/components/Links.js
+++ src/components/Links.js
@@ -10,13 +10,13 @@ const Links = () => {
   return (
     <div className="Links">
       <a href="mailto:foreverchoi0706@gmail.com" target="blank">
-        <img src={blog} alt="blog" />
+        <img src={email} alt="blog" />
       </a>
       <a href="https://github.com/foreverchoi0706" target="blank">
         <img src={github} alt="github" />
       </a>
       <a href="https://velog.io/@foreverchoi0706" target="blank">
-        <img src={email} alt="email" />
+        <img src={blog} alt="email" />
       </a>
     </div>
   );
