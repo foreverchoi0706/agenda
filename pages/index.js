ba5811690f09e35d5c80cd98c492c554ed1cf2f6
foreverChoi <foreverchoi0706@gmail.com>
2021-02-20 21:18:38 +0900

    re

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="./views/Count" >sad</Link>
+    </div>
+  )
+}
--- src/components/Skills/Skill.js
+++
@@ -1,7 +0,0 @@
-import React, { memo } from "react";
-
-const Skill = ({ name, src, description }) => {
-  return <img className="Skill" src={src} alt={name} />;
-};
-
-export default memo(Skill);
