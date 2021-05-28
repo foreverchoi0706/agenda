8881721e752031545966d2e5a3bdc3baf90602e1
foreverChoi <foreverchoi0706@gmail.com>
2021-05-28 12:57:20 +0900

    empty

--- README.md
+++
@@ -1,5 +0,0 @@
-# GO NOW 
-  [click](https://portfolio.foreverchoi0706.vercel.app/)
-
-# DEPLOYED BY
-  [click](https://vercel.com/foreverchoi0706)
--- pages/components/Footer.js
+++
@@ -1,7 +0,0 @@
-import styles from "../../styles/components/Footer.module.scss";
-
-const Footer = () => {
-  return <footer className={styles.Footer}>foreverchoi0706@gmail.com</footer>;
-};
-
-export default Footer;
