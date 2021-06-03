cb94894841d9ad2910b49deb2d1092b36a88a387
foreverChoi <foreverchoi0706@gmail.com>
2021-06-03 19:34:07 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import root from "./reducers/root";
 import { BrowserRouter, Route } from "react-router-dom";
 //pages
 import Home from "./pages/Home";
@@ -7,20 +10,24 @@ import About from "./pages/About";
 //components
 import Layout from "./components/Layout";
 
+const store = createStore(root);
+
 const App = () => {
   const [isLogined, setIsLogined] = useState(true);
 
   return (
-    <BrowserRouter>
-      {isLogined ? (
-        <Route path="/" component={SignIn} exact />
-      ) : (
-        <Layout>
-          <Route path="/home" component={Home} exact />
-          <Route path="/about" component={About} exact />
-        </Layout>
-      )}
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        {isLogined ? (
+          <Route path="/" component={SignIn} exact />
+        ) : (
+          <Layout>
+            <Route path="/home" component={Home} exact />
+            <Route path="/about" component={About} exact />
+          </Layout>
+        )}
+      </BrowserRouter>
+    </Provider>
   );
 };
 
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,8 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
-import { createStore } from "redux";
-import { Provider } from "react-redux";
-import root from "./reducers/root";
-
 import "../styles/global.css";
 
 const agenda = document.querySelector("#agenda");
-
-const store = createStore(root);
-
-const app = () => {
-    return <Provider store={ store }> <App/></Provider >
-}
-
 
 ReactDom.render(React.createElement(App), agenda);
